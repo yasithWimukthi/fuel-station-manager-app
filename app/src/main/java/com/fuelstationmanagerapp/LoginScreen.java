1a5240adcee10fb5fc966527a6707f8186f364f9
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-23 03:42:54 +0530

    added initiviews to login

--- app/src/main/java/com/fuelstationmanagerapp/LoginScreen.java
+++ app/src/main/java/com/fuelstationmanagerapp/LoginScreen.java
@@ -1,14 +1,60 @@
 package com.fuelstationmanagerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class LoginScreen extends AppCompatActivity {
+import com.fuelstationmanagerapp.helpers.InputValidation;
+import com.fuelstationmanagerapp.model.User;
+import com.fuelstationmanagerapp.sql.DatabaseHelper;
+
+public class LoginScreen extends AppCompatActivity implements View.OnClickListener{
+
+    private final AppCompatActivity activity = RegisterScreen.this;
+
+    private TextView emailErrors;
+    private TextView passwordErrors;
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private Button buttonRegister;
+    private AppCompatTextView appCompatTextViewLoginLink;
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_screen);
+        setContentView(R.layout.activity_register_screen);
+        getSupportActionBar().hide();
+
+        initViews();
+        initListeners();
+        initObjects();
+    }
+
+    /**
+     * This method is to initialize views
+     */
+    private void initViews() {
+        emailErrors = (TextView) findViewById(R.id.emailErrors);
+        passwordErrors = (TextView) findViewById(R.id.passwordErrors);
+
+        emailErrors.setVisibility(View.GONE);
+        passwordErrors.setVisibility(View.GONE);
+
+        editTextEmail = (EditText) findViewById(R.id.inputEmail);
+        editTextPassword = (EditText) findViewById(R.id.inputPassword);
+
+        buttonRegister = (Button) findViewById(R.id.btnRegister);
+//      appCompatTextViewLoginLink = (AppCompatTextView) findViewById(R.id.appCompatTextViewLoginLink);
     }
 }
