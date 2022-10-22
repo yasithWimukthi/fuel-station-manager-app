5821f180629592dec92ace020150fe06d2ac31ae
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-22 23:04:11 +0530

    added new class for input validation

---
+++ app/src/main/java/com/fuelstationmanagerapp/helpers/InputValidation.java
@@ -0,0 +1,18 @@
+package com.fuelstationmanagerapp.helpers;
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+
+public class InputValidation {
+    private Context context;
+    /**
+     * constructor
+     *
+     * @param context
+     */
+    public InputValidation(Context context) {
+        this.context = context;
+    }
+}
