b7bf0375515207da62a31354b353c64e61bc969d
yasith <it19966922@my.sliit.lk>
2022-10-22 12:17:26 +0530

    create register screen

--- app/src/main/java/com/fuelstationmanagerapp/MainActivity.java
+++ app/src/main/java/com/fuelstationmanagerapp/MainActivity.java
@@ -1,14 +1,141 @@
 package com.fuelstationmanagerapp;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.yarolegovich.slidingrootnav.SlidingRootNav;
+import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity  implements DrawerAdapter.OnItemSelectedListener{
+
+    private static final int POS_CLOSE = 0;
+    private static final int POS_DASHBOARD = 1;
+    private static final int POS_LOGOUT = 2;
+    private static final String TAG = "Main Activity";
+
+    private String[] screenTitles;
+    private Drawable[] screenIcons;
+
+    private SlidingRootNav slideRootNav;
+    private BottomNavigationView bottomNavBar;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        try{
+            setSupportActionBar(toolbar);
+        }catch(IllegalStateException e){
+            Log.e(TAG, "onCreate: "+ e.getMessage() );
+        }
+
+        slideRootNav = new SlidingRootNavBuilder(this)
+                .withDragDistance(180)
+                .withRootViewScale(0.75f)
+                .withRootViewElevation(25)
+                .withToolbarMenuToggle(toolbar)
+                .withMenuOpened(false)
+                .withContentClickableWhenMenuOpened(false)
+                .withSavedState(savedInstanceState)
+                .withMenuLayout(R.layout.drawer_menu)
+                .inject();
+
+        screenIcons = loadScreenIcons();
+        screenTitles = loadScreenTitles();
+
+
+        DrawerAdapter adapter = new DrawerAdapter(Arrays.asList(
+                createItemFor(POS_CLOSE),
+                createItemFor(POS_DASHBOARD),
+                new SpaceItem(150),
+                createItemFor(POS_LOGOUT)
+        ));
+
+        adapter.setListener(this);
+
+        RecyclerView list = findViewById(R.id.drawer_list);
+        list.setNestedScrollingEnabled(false);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        list.setAdapter(adapter);
+
+        adapter.setSelected(POS_DASHBOARD);
+    }
+
+    private DrawerItem createItemFor(int position){
+        return new SimpleItem(screenIcons[position],screenTitles[position])
+                .withIconTint(color(R.color.colorPrimary))
+                .withTextTint(color(R.color.black))
+                .withSelectedIconTint(color(R.color.colorPrimary))
+                .withSelectedTextTint(color(R.color.colorPrimary));
+    }
+
+    @ColorInt
+    private int color(@ColorRes int res){
+        return ContextCompat.getColor(this,res);
+    }
+
+
+    private String[] loadScreenTitles() {
+        return getResources().getStringArray(R.array.id_activityScreenTitles);
+    }
+
+    private Drawable[] loadScreenIcons() {
+        TypedArray ta = getResources().obtainTypedArray(R.array.id_activityScreenIcons);
+        Drawable[] icons = new Drawable[ta.length()];
+
+        for(int i=0 ; i<ta.length() ; i++) {
+            int id = ta.getResourceId(i,0);
+            if (id != 0){
+                icons[i] = ContextCompat.getDrawable(this,id);
+            }
+        }
+
+        ta.recycle();
+        return icons;
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+
+    @Override
+    public void onItemSelected(int position) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        if (position == POS_DASHBOARD){
+            toolbar.setTitle("Dashboard");
+            HomeFragment homeFragment = new HomeFragment();
+            transaction.replace(R.id.container, homeFragment);
+        }else{
+            finish();
+        }
+
+        slideRootNav.closeMenu();
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
