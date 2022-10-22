a096ab4be045c04bd32ab8d3ced8824c90a104db
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-22 22:54:44 +0530

    created database helper class

---
+++ app/src/main/java/com/fuelstationmanagerapp/sql/DatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.fuelstationmanagerapp.sql;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.fuelstationmanagerapp.model.User;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper {
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+    // Database Name
+    private static final String DATABASE_NAME = "UserManager.db";
+    // User table name
+    private static final String TABLE_USER = "user";
+    // User Table Columns names
+    private static final String COLUMN_USER_ID = "user_id";
+    private static final String COLUMN_USER_NAME = "user_name";
+    private static final String COLUMN_USER_EMAIL = "user_email";
+    private static final String COLUMN_USER_PASSWORD = "user_password";
+    // create table sql query
+    private String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USER + "("
+            + COLUMN_USER_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + " TEXT,"
+            + COLUMN_USER_EMAIL + " TEXT," + COLUMN_USER_PASSWORD + " TEXT" + ")";
+    // drop table sql query
+    private String DROP_USER_TABLE = "DROP TABLE IF EXISTS " + TABLE_USER;
+}
