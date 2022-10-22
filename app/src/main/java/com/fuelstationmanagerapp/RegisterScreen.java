a8bbe4904feef375df9a3388398179b0a1074b9b
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-22 23:52:40 +0530

    added initViews method to registerScreen activity

--- app/src/main/java/com/fuelstationmanagerapp/RegisterScreen.java
+++ app/src/main/java/com/fuelstationmanagerapp/RegisterScreen.java
@@ -1,14 +1,63 @@
 package com.fuelstationmanagerapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.core.widget.NestedScrollView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.fuelstationmanagerapp.helpers.InputValidation;
+import com.fuelstationmanagerapp.model.User;
+import com.fuelstationmanagerapp.sql.DatabaseHelper;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterScreen extends AppCompatActivity {
 
+    private final AppCompatActivity activity = RegisterScreen.this;
+
+    private NestedScrollView nestedScrollView;
+
+    private TextInputLayout textInputLayoutEmail;
+    private TextInputLayout textInputLayoutPassword;
+    private TextInputLayout textInputLayoutConfirmPassword;
+
+    private EditText textInputEditTextEmail;
+    private EditText textInputEditTextPassword;
+    private EditText textInputEditTextConfirmPassword;
+
+    private Button buttonRegister;
+    private AppCompatTextView appCompatTextViewLoginLink;
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
+    private User user;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+        getSupportActionBar().hide();
+
+        initViews();
+        initListeners();
+        initObjects();
+    }
+
+    /**
+     * This method is to initialize views
+     */
+    private void initViews() {
+//        nestedScrollView = (NestedScrollView) findViewById(R.id.nestedScrollView);
+
+        textInputEditTextEmail = (EditText) findViewById(R.id.inputEmail);
+        textInputEditTextPassword = (EditText) findViewById(R.id.inputPassword);
+        textInputEditTextConfirmPassword = (EditText) findViewById(R.id.confirmPassword);
+
+        buttonRegister = (Button) findViewById(R.id.btnRegister);
+//        appCompatTextViewLoginLink = (AppCompatTextView) findViewById(R.id.appCompatTextViewLoginLink);
     }
 }
