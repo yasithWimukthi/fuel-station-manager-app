20950bc50e88c839d4b27c52df4c2d2774a1509b
yasith <it19966922@my.sliit.lk>
2022-10-23 22:26:43 +0530

    create home screen

--- app/src/main/java/com/fuelstationmanagerapp/FuelStationAdapter.java
+++ app/src/main/java/com/fuelstationmanagerapp/FuelStationAdapter.java
@@ -1,7 +1,9 @@
 package com.fuelstationmanagerapp;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,13 +12,32 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.fuelstationmanagerapp.model.FuelStation;
 
-public class FuelStationAdapter {
+public class FuelStationAdapter extends RecyclerView.Adapter<FuelStationAdapter.ViewHolder>{
     private Context context;
     private FuelStation[] fuelStations;
 
     public FuelStationAdapter(Context context, FuelStation[] fuelStations) {
         this.context = context;
         this.fuelStations = fuelStations;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.fuel_station_list_item,parent,false);
+        FuelStationAdapter.ViewHolder viewHolder = new FuelStationAdapter.ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
