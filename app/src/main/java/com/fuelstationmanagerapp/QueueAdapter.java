40128208c864dd14df99769276c75ccb659c1773
yasith <it19966922@my.sliit.lk>
2022-10-23 18:18:07 +0530

    create home screen

--- app/src/main/java/com/fuelstationmanagerapp/QueueAdapter.java
+++ app/src/main/java/com/fuelstationmanagerapp/QueueAdapter.java
@@ -1,17 +1,30 @@
 package com.fuelstationmanagerapp;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.fuelstationmanagerapp.model.QueueItem;
+
 public class QueueAdapter extends RecyclerView.Adapter<QueueAdapter.ViewHolder> {
 
+    Context context;
+    QueueItem[] queueItems;
+
     public QueueAdapter() {
+
+    }
+
+    public QueueAdapter(Context context, QueueItem[] queueItems) {
+        this.context = context;
+        this.queueItems = queueItems;
     }
 
     @NonNull
@@ -25,12 +38,23 @@ public class QueueAdapter extends RecyclerView.Adapter<QueueAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final QueueItem queueItem = queueItems[position];
+        holder.textViewName.setText(queueItem.getName());
+        holder.textViewDate.setText(queueItem.getDate());
+//        holder.statusImageView.setImageResource(queueItem.getMovieImage());
+        holder.textViewStatus.setText(queueItem.getStatus());
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, queueItem.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return queueItems.length;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
