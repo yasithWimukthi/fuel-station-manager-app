d3489f50c37b2d06a6ae055664e94d33d66d32c8
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-25 22:11:38 +0530

    added list of customers to fuelqueue db model

--- app/src/main/java/com/fuelstationmanagerapp/dbModel/FuelQueue.java
+++ app/src/main/java/com/fuelstationmanagerapp/dbModel/FuelQueue.java
@@ -1,5 +1,7 @@
 package com.fuelstationmanagerapp.dbModel;
 
+import java.util.ArrayList;
+
 public class FuelQueue {
 
     private String fuelStationName;
@@ -7,5 +9,5 @@ public class FuelQueue {
     private String vehicleType;
     private String fuelStatus;
     private int count;
-
+    private ArrayList<Customer> customers;
 }
