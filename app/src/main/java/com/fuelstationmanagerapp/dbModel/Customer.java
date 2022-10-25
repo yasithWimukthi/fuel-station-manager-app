58b61403f86d630b38f477984daf394e8dc73841
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-25 22:51:03 +0530

    updated customer db model

--- app/src/main/java/com/fuelstationmanagerapp/dbModel/Customer.java
+++ app/src/main/java/com/fuelstationmanagerapp/dbModel/Customer.java
@@ -6,4 +6,32 @@ public class Customer {
     private String arrivalTime;
     private String departTime;
     private String status;
+
+    public Customer(String _id, String customerName, String arrivalTime, String departTime, String status) {
+        this._id = _id;
+        this.customerName = customerName;
+        this.arrivalTime = arrivalTime;
+        this.departTime = departTime;
+        this.status = status;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public String getDepartTime() {
+        return departTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
