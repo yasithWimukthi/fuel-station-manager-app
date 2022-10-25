468086798a33abba214d291088c9e173dce8f62e
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-25 22:09:31 +0530

    added new db model for customer

---
+++ app/src/main/java/com/fuelstationmanagerapp/dbModel/Customer.java
@@ -0,0 +1,9 @@
+package com.fuelstationmanagerapp.dbModel;
+
+public class Customer {
+    private String _id;
+    private String customerName;
+    private String arrivalTime;
+    private String departTime;
+    private String status;
+}
