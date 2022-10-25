2559783e4fd51235c30f6acc492e6a1cbab7066d
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-25 22:06:34 +0530

    created model for fuelQueue

---
+++ app/src/main/java/com/fuelstationmanagerapp/dbModel/FuelQueue.java
@@ -0,0 +1,11 @@
+package com.fuelstationmanagerapp.dbModel;
+
+public class FuelQueue {
+
+    private String fuelStationName;
+    private String fuelType;
+    private String vehicleType;
+    private String fuelStatus;
+    private int count;
+
+}
