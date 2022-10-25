c36878a2c48e31bfca41ff40863770af1c648066
Hashen Kottegoda <thkottegoda@gmail.com>
2022-10-25 22:49:16 +0530

    updated fuelQueue model

--- app/src/main/java/com/fuelstationmanagerapp/dbModel/FuelQueue.java
+++ app/src/main/java/com/fuelstationmanagerapp/dbModel/FuelQueue.java
@@ -10,4 +10,37 @@ public class FuelQueue {
     private String fuelStatus;
     private int count;
     private ArrayList<Customer> customers;
+
+    public FuelQueue(String fuelStationName, String fuelType, String vehicleType, String fuelStatus, int count, ArrayList<Customer> customers) {
+        this.fuelStationName = fuelStationName;
+        this.fuelType = fuelType;
+        this.vehicleType = vehicleType;
+        this.fuelStatus = fuelStatus;
+        this.count = count;
+        this.customers = customers;
+    }
+
+    public String getFuelStationName() {
+        return fuelStationName;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public String getFuelStatus() {
+        return fuelStatus;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
 }
