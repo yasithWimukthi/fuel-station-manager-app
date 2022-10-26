5a4c8088538a1e1d0e2bea1de7b5a65349f6bfbc
yasith <it19966922@my.sliit.lk>
2022-10-26 19:55:29 +0530

    change queue list item

--- app/src/main/java/com/fuelstationmanagerapp/UpdateFuelStatusFragment.java
+++ app/src/main/java/com/fuelstationmanagerapp/UpdateFuelStatusFragment.java
@@ -81,6 +81,7 @@ public class UpdateFuelStatusFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
